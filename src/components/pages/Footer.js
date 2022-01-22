29dde350e19139d0d9624f500195a642df51543c
Muhammet <mkarakoc@live.com>
2022-01-22 16:19:45 -0500

    First commit

---
+++ src/components/pages/Footer.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import Button from "../Button";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <section className="footer-subscription">
+        <p className="footer-subscription-heading">
+          Join the Adcenture newsletter to receive our best vacation deals
+        </p>
+        <p className="footer-subscription-text">
+          You can unsubscribe at any time
+        </p>
+        <div className="input-areas">
+          <form>
+            <input
+              type="email"
+              name="email"
+              placeholder="Your Email"
+              className="footer-input"
+            />
+            <Button buttonStyle="btn--outline"> Subscribe</Button>
+          </form>
+        </div>
+      </section>
+      <div className="footer-links">
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>About Us</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Term of Services</Link>
+          </div>
+          <div className="footer-link-items">
+            <h2>About Us</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Term of Services</Link>
+          </div>
+        </div>
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>About Us</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Term of Services</Link>
+          </div>
+          <div className="footer-link-items">
+            <h2>About Us</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Term of Services</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
